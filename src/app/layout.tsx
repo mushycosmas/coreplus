5025c50afc9fb8dc1af7fe60a9f9a83216181b45
mushycosma <kevomassawe@gmail.com>
2025-08-24 12:08:40 +0300

    fix:data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,8 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: React.PropsWithChildren<{}>) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
